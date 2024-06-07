b52962f3deb7a92b9353a9ccc706882aff3f1513
Josue Amador Ynfante <159919309+jossamador@users.noreply.github.com>
2024-06-06 19:23:40 -0600

    Delete ProyectoJ directory

--- ProyectoJ/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const dropdownMenu = document.getElementById('dropdown-menu');
-
-    menuIcon.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('show');
-    });
-});
